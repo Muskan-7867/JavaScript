4fa24b5a9c1fd88e26e379a3e918fead5a8337e7
muskan <muskanloach984@gmail.com>
2023-12-18 23:29:13 +0530

    app.js deleted

--- javaScript/app.js
+++
@@ -1,4 +0,0 @@
-console.log("hello mj madam node instaall nhi kita ")
-jo three circle vala icon aa ye haega  git nal connect tu jo change krna enne show krna ki kuchh change hoiya
-
-je koi new file v bnaungi o v ethe show hojuga ok
