c51621cbf221c26ebd9b04efa6be0327ec4f28e5
muskan <muskanloach984@gmail.com>
2024-01-26 22:07:54 +0530

    objects

---
+++ objects/array.js
@@ -0,0 +1,115 @@
+// inheritance
+// class parent {
+
+//     motherfather(){
+//         console.log("my parents");
+//     }
+
+//     grandparents() {
+//         console.log("my grandparents");
+//     }
+// }
+
+// class student extends parent {
+    
+//     study() {
+//         console.log("i am studying");
+//     }
+// }
+
+// let muskan = new student();
+
+
+// const deepakobj = {
+//     fullname :"Mohan  kumar",
+//     age : "10",
+//     course : "btech",
+//     gender : "male"
+
+// }
+// console.log(`Fullname is:${deepakobj.fullname}`);
+// console.log(`age is :${deepakobj.age}`);
+// console.log(`course is: ${deepakobj.course}`);
+// console.log(`gender is: ${deepakobj.gender}`);
+
+
+// array exercise
+
+// let productsObject = [
+//     {productName : "Mac",price :1000},
+
+//     {productName : "HP",price : 15000},
+    
+//     {productName : "dell",price : 5000}
+// ];
+
+// console.log(productsObject);
+
+// class  Car {
+//     start() {
+//        console.log("start");
+//     }
+    
+//     stop() {
+//         console.log("stop");
+//     }
+
+//     setBrand(brand) {
+//         this.brand = brand;
+//     }
+// }
+//  let fortuner = new Car();
+//  fortuner.setBrand("fortuner");
+
+
+// JSON PRACTICE
+
+// let product ={
+//     Name : "Zara",
+//     price : 785,
+//     rating:{
+//        stars : 3.6
+//     }
+
+// };
+// console.log(typeof product)
+// console.log(product)
+// let string =JSON.stringify(product)
+// console.log(typeof string)
+// console.log(string)
+
+// let newString = JSON.parse(string)
+// console.log(newString)
+// console.log( typeof newString)
+
+// exerxise
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
